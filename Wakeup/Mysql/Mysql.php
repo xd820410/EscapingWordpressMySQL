9d1c7ee8e366704b4f7274b5a93dda0002fed859
YCWang <xd8204102@gmail.com>
2020-10-26 17:35:48 +0800

    init && dev package

---
+++ Wakeup/Mysql/Mysql.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Wakeup\Mysql;
+
+class Mysql
+{
+    private $dbHost = '';
+    private $dbName = '';
+    private $dbUser = '';
+    private $dbPassword = '';
+    private $connection;
+
+    public function __construct()
+    {
+        $this->getWordpressDatabaseInfomation();
+        $this->sqlConnect();
+    }
+
+    private function getWordpressDatabaseInfomation()
+    {
+        $file = fopen($_SERVER['DOCUMENT_ROOT'] . '/wp-config.php', 'r');
+
+        if (!empty($file)) {
+
+            while (!feof($file)) {
+                $line = (string) fgets($file);
+
+                if (strpos($line, 'dbName')) {
+                    $targetSentence = explode(',', $line);
+                    $targetValue = explode('\'', $targetSentence[1]);
+                    $dbName = $targetValue[1];
+                }
+
+                if (strpos($line, 'dbUser')) {
+                    $targetSentence = explode(',', $line);
+                    $targetValue = explode('\'', $targetSentence[1]);
+                    $dbUser = $targetValue[1];
+                }
+
+                if (strpos($line, 'dbPassword')) {
+                    $targetSentence = explode(',', $line);
+                    $targetValue = explode('\'', $targetSentence[1]);
+                    $dbPassword = $targetValue[1];
+                }
+
+                if (strpos($line, 'dbHost')) {
+                    $targetSentence = explode(',', $line);
+                    $targetValue = explode('\'', $targetSentence[1]);
+                    $dbHost = $targetValue[1];
+                }
+            }
+            fclose($file);
+
+            $this->dbHost = $dbHost;
+            $this->dbName = $dbName;
+            $this->dbUser = $dbUser;
+            $this->dbPassword = $dbPassword;
+        }
+    }
+
+    private function sqlConnect()
+    {
+        $this->connection = new PDO('mysql: host = ' . $this->dbHost . '; dbname = ' . $this->dbName . '; charset=utf8', $this->dbUser, $this->dbPassword);
+    }
+}
